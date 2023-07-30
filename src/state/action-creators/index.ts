44b47f18132c58b6d778b59e1ba6383ab472edcb
Felis Sedano <sedanofelis@gmail.com>
2023-07-29 22:05:31 -0400

    added comments

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -14,6 +14,7 @@ export const searchRepos = (term: string, repoManagerSite: string) => {
             console.log("about to search repos");
 
             switch (repoManagerSite) {
+                // Search using cargo api
                 case RepoMangerType.CARGO: {
                     const { data } = await axios.get(
                         'https://crates.io/api/v1/crates',
@@ -34,6 +35,7 @@ export const searchRepos = (term: string, repoManagerSite: string) => {
                     break;
                 }
 
+                // Search using npm api
                 case RepoMangerType.NPM: {
                     const { data } = await axios.get(
                         'http://registry.npmjs.org/-/v1/search',
@@ -57,44 +59,8 @@ export const searchRepos = (term: string, repoManagerSite: string) => {
 
 
 
-
-
             }
-            // const { data } = await axios.get(
-            //     'https://crates.io/api/v1/crates',
-            //     {
-            //         params: {
-            //             q: term
-            //         }
-            //     }
-            // );
 
-            // console.log(data);
-
-
-
-
-            // const { data } = await axios.get(
-            //     'http://registry.npmjs.org/-/v1/search',
-            //     {
-            //         params: {
-            //             text: term
-            //         }
-            //     }
-            // );
-
-            // const names = data.crates.map((result: any) => {
-            //     return result.name;
-            // });
-
-            // const names = data.objects.map((result: any) => {
-            //     return result.package.name;
-            // });
-
-            // dispatch({
-            //     type: ActionType.SEARCH_REPOS_SUCCESS,
-            //     payload: names
-            // });
 
         } catch (error: any) {
             console.log("error happened, the messagge is " + error.message);
