db34e61e17356368dbd6729f04ed8027a3971c58
Felis Sedano <sedanofelis@gmail.com>
2023-08-28 19:17:12 -0400

    modification of content display of html

--- src/components/RepoWidget.tsx
+++ src/components/RepoWidget.tsx
@@ -1,15 +1,19 @@
 import { ReposData } from "../state/actions";
+import "./RepoWidget.css"
 
 const RepoWidget: React.FC<ReposData> = (reposData: ReposData) => {
-    return <div>
-        {reposData.githubAvatarLink && <img src={reposData.githubAvatarLink} alt="avatar" />}
-        <div>
-            <p>{reposData.authorName}</p>
-            <ul>
-                <li>{reposData.repoName}</li>
-                <li>{reposData.authorName}</li>
-                <li>{reposData.githubLink}</li>
-                <li>{reposData.githubStarredCount}</li>
+    return <div className="container__repowidget">
+        {reposData.githubAvatarLink && <img className="image__avatar" src={reposData.githubAvatarLink} alt="avatar" />}
+        <div className="container__repoinfo--main">
+            
+            <h3>{reposData.repoName}</h3>
+        </div>
+        <div className="container__repoinfo--extra">
+            {/* <p>{reposData.authorName}</p> */}
+            <ul className="list__repoinfo">
+                <li>Author: {reposData.authorName}</li>
+                <li>GitHub Starred Count : {reposData.githubStarredCount}</li>
+                <li> <a href={reposData.githubLink} target="_blank" rel="noreferrer">{reposData.githubLink}</a> </li>
             </ul>
         </div>
     </div>
